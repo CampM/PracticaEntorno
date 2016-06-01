3155e9c845d2d088ad7a56b9d825d37151f24c46
Moises Campon <Sesiom_88@hotmail.es>
2016-06-01 20:01:28 +0200

    Se ha añadido un label a Form1 con el texto PROYECTO DE MOISES

--- Problema2/WindowsFormsApplication1/Form1.Designer.cs
+++ Problema2/WindowsFormsApplication1/Form1.Designer.cs
@@ -32,6 +32,7 @@
             this.lbContrseña = new System.Windows.Forms.Label();
             this.lbPista = new System.Windows.Forms.Label();
             this.btnAceptar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // tbContraseña
@@ -70,12 +71,23 @@
             this.btnAceptar.UseVisualStyleBackColor = true;
             this.btnAceptar.Click += new System.EventHandler(this.btnAceptar_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label1.Location = new System.Drawing.Point(83, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "PROYECTO DE MOISES";
+            // 
             // Form1
             // 
             this.AcceptButton = this.btnAceptar;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(290, 277);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnAceptar);
             this.Controls.Add(this.lbPista);
             this.Controls.Add(this.lbContrseña);
@@ -93,6 +105,7 @@
         private System.Windows.Forms.Label lbContrseña;
         private System.Windows.Forms.Label lbPista;
         private System.Windows.Forms.Button btnAceptar;
+        private System.Windows.Forms.Label label1;
     }
 }
 
